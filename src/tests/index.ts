cf64740c1c0bcca51c422d550d2b929e09af4c28
stanley <stanley@zhihesoft.com>
2022-08-15 18:28:45 +0800

    ignore test file when package

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,8 +1,15 @@
 import { assert } from "chai";
-import { checkDir } from "../lib/util";
+import { checkDir, ensureDir, getExcels, removeDir } from "../lib/util";
 
 describe(`test suit`, () => {
     it(`util`, async () => {
+        removeDir("./dist/temp");
         assert.isTrue(checkDir("./dist"));
+        assert.equal(getExcels("./src/tests/excel").length, 4);
+        assert.isFalse(checkDir("./dist/temp"));
+        ensureDir("./dist/temp");
+        assert.isTrue(checkDir("./dist/temp"), "temp dir should created");
+        removeDir("./dist/temp");
+        assert.isFalse(checkDir("./dist/temp"), "temp dir should be removed");
     });
 });
